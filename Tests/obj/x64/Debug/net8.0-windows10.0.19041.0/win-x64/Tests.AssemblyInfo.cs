492acef2c77d27d3a4715e7c75680f79ee9ca185
Sampalean Sorin <sampaleansorin@outlook.com>
2025-04-10 21:57:24 +0300

    Added all the tests

--- Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
+++ Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8bebd1c06e2810cde28fdc10baa62778c39151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e542bf02e36f5aac620084fea997a614acf64d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
