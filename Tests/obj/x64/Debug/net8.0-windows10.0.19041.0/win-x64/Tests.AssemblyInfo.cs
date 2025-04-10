9fb86c9e2b3e937d63c12851972277e9ea07ae6e
saba-calin <sabaila.calin10@gmail.com>
2025-04-10 07:02:57 -0700

    initial commit

--- Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
+++ Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d09bff7632288368dcd77923e0a0b5e795600b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463c72d1d348da482df72fcc18d216a35158c815")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
