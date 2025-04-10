237b25b0f03758fa4f3a5a494bd410e6c2a37947
Sampalean Sorin <sampaleansorin@outlook.com>
2025-04-10 19:09:03 +0300

    Added the tests

--- Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
+++ Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
@@ -16,7 +16,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d09bff7632288368dcd77923e0a0b5e795600b1")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41eac0abf600d892dd072cd234c0b4411a17f783")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41eac0abf600d892dd072cd234c0b4411a17f783")]
 >>>>>>> Stashed changes
