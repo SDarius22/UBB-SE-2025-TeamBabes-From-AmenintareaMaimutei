015ceffac1307fec0f9f7013d160a29dd3dce159
Sala Darius-George <saladarius72@gmail.com>
2025-04-11 11:00:25 +0300

    config changed on my machine

--- Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
+++ Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85cee18a9f060f9790d081f653858a4b47cd84b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ed9dad77eac83dd7698c6996599da557c8bb06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
